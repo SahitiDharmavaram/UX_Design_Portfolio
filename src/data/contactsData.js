5de7c4091daff54d094de6bcc75ab9aa29a1a1d6
SahitiDharmavaram <sahiti.dharmavaram2021@vitstudent.ac.in>
2023-11-01 21:04:08 +0530

    files added

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,6 +1,5 @@
 export const contactsData = {
     email: 'sahiti.dharmavaram2021@vitstudent.ac.in',
-    phone: '+919513808057',
     address: 'Bangalore, India',
 
     sheetAPI: ''
--- src/data/educationData.js
+++ src/data/educationData.js
@@ -6,19 +6,5 @@ export const educationData = [
         startYear: '2021',
         endYear: 'Present'
     },
-    {
-        id: 2,
-        institution: 'Narayna E-techno School, Bangalore',
-        course: 'Higher Secondary Education',
-        startYear: '2019',
-        endYear: '2021'
-    },
-    {
-        id: 1,
-        institution: 'Delhi Public School- Bangalore North',
-        course: 'Primary + Secondary Education',
-        startYear: '2007',
-        endYear: '2019'
-    },
-
+    
 ]

37281e9c42e2155850d9d84f226aeb292aef9f7c
SahitiDharmavaram <sahiti.dharmavaram2021@vitstudent.ac.in>
2023-11-01 19:34:01 +0530

    files added

---
+++ src/data/contactsData.js
@@ -0,0 +1,7 @@
+export const contactsData = {
+    email: 'sahiti.dharmavaram2021@vitstudent.ac.in',
+    phone: '+919513808057',
+    address: 'Bangalore, India',
+
+    sheetAPI: ''
+}
---
+++ src/data/educationData.js
@@ -0,0 +1,24 @@
+export const educationData = [
+    {
+        id: 3,
+        institution: 'Vellore Institute of Technology, Vellore',
+        course: 'B.Tech Computer Science and Engineering',
+        startYear: '2021',
+        endYear: 'Present'
+    },
+    {
+        id: 2,
+        institution: 'Narayna E-techno School, Bangalore',
+        course: 'Higher Secondary Education',
+        startYear: '2019',
+        endYear: '2021'
+    },
+    {
+        id: 1,
+        institution: 'Delhi Public School- Bangalore North',
+        course: 'Primary + Secondary Education',
+        startYear: '2007',
+        endYear: '2019'
+    },
+
+]

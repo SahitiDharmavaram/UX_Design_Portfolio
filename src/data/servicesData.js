201920a4aa5c4953e5fdd1c67bf082fe3eefdedd
Ryan Hughes <ryanhughes624@gmail.com>
2023-11-02 16:41:53 -0400

    .

--- src/data/servicesData.js
+++ src/data/servicesData.js
@@ -2,6 +2,7 @@
 import { BiShoppingBag, BiPencil } from "react-icons/bi";
 import { BsCodeSlash, BsClipboardData } from "react-icons/bs";
 import { AiOutlineMail, AiFillAudio } from "react-icons/ai";
+import React from 'react';
 import { FaInternetExplorer, FaChalkboardTeacher, FaCameraRetro, FaPinterest, FaVideo, FaTabletAlt, FaRegNewspaper } from "react-icons/fa";
 
 // Import the PNG files
